397c9ab2504ad7d3539266a7c2c38984d8991b9f
unknown <Jgossett73@gmail.com>
2022-08-25 23:58:27 -0400

    schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,58 @@
 const {AuthenticationError} = require('apollo-server-express');
-const { saveBook } = require('../controllers/user-controller');
 const {User} = require('../models');
 const {signToken} = require ('../utils/auth');
 
 const resolvers = {
-    Query:{
-        users: async () => {
-            return User.find().populate('books');
-},
-        user: async (parent,{username}) => {
-            return User.findone({username}).populate('books');
-},
-        books:async (parent,{username}) => {
-            const params = username? {username}:{};
-            return Book.find(params).sort({createdAt:-1});
-},
-        book: async (parent,{bookId}) => {
-            return Book.findone({_id:bookId});
+    Query:{ 
+        me: async (parent,args,context => {
+            if(context.user) {
+            const.userData = await User.findOne({_id:AudioContext.user_id}).select('-_v -password');
+            return userData; 
 }
-},            
+            throw new AuthenticationError('Must be logged in!');
 },
-  
-        
-
+},
+    Mutation: {
+        addUser: async(parent,args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token,user};
+        },
+        login: async(parent, {email,password}) => {
+            const user = await User.findOne({email});
+            if(!user) {
+                throw new AuthenticationError('User not found');
+            }
+            const correctPass = await user.isCorrectPassword(password);
+            if(!correctPass) {
+                throw new AuthenticationError('Didnt say the magic word!');
+            }
+            const token = signToken(user);
+            return {token};
+        },
+        saveBook: async(parent, {newBook},context) => {
+            if(context.user) {
+                const updateUser = await User.findByIdAndUpdate(
+                    {_id:context.user._id},
+                    {$push:{savedBooks:newBook}},
+                    {new:true}
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('Didnt say the magic word');
+        },
+        removeBook: async(parent, {bookId}, context) => {
+            if(context.user) {
+                const updateUser = await User.findByIdAndUpdate(
+                    {_id:context.user._id},
+                    {$pull:{savedBooks:{bookId}}},
+                    {new:true}
+                );
+                return updateUser;
+            }
+                throw new AuthenticationError('Didnt say the magic word');
+            },
+        },
+    };
+    module.exports = resolvers;
     
