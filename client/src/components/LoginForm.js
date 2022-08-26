5b0363dd74505999a94b3d6767962ca4ddc75110
unknown <Jgossett73@gmail.com>
2022-08-25 21:41:20 -0400

    login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,19 +2,28 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/mutations';
-import {useMutation} from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
+import {useMutation} from '@apollo/client';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login,{error}] = useMutation(LOGIN_USER);
 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    }else{
+      setShowAlert(false);
+    }
+  },[error]);
+  
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
-  };
+};
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -24,25 +33,18 @@ const LoginForm = () => {
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
-    }
+}
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const {data} = await login({variables:{...userFormData}}
+);
+        Auth.login(data.login.token);
+      } catch (err){
+        console.error(err); 
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+ 
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
---
+++ client/src/utils/queries.js
@@ -0,0 +1,6 @@
+import {gql} from '@apollo/client';
+export const GET_ME = gql`
+{me
+{_id username email bookCount savedBooks
+{bookId authors description title image link}}
+}`;
