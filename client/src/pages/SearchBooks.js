5c88130666f73076e6363dcd01a7a09365771a80
unknown <Jgossett73@gmail.com>
2022-08-25 22:32:35 -0400

    deleted routes

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -13,13 +13,12 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
+  const [saveBook, {error}] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
-
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -75,7 +74,7 @@ const SearchBooks = () => {
     catch(err){
       console.error(err);
     }};
-    
+
     return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
