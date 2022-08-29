0aca56b831f358f364af011330967b53efc2e45e
unknown <Jgossett73@gmail.com>
2022-08-29 16:51:30 -0400

    src folders json and routing functions

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -6,38 +6,69 @@ mutation login($email:String! $password:String!)
 {
   login(email:$email password:$password)
 {
-  token user {_id username}
+  token user
+   {
+    _id 
+    username 
+  email
+}
 }  
 }`;
 
 export const ADD_USER = gql`
 mutation addUser($username:String! $email:String! $password:String!)
 {
-  addUser(username:$username email:$email password:$password)
+  addUser(username:$username email:$email password:$password,email:$email)
   {
-    token user {_id username email bookCount savedBooks
-    {
-      authors bookId image link title description
-    }}
+ user {
+  _id 
+  username 
+  email
   }
-}`;
+  token 
+  }}
+  `;
     
 // save book data for a logged in user
 export const SAVE_BOOK = gql`
-mutation saveBook($newBook:InputBook!)
+mutation saveBook(bookData$bookData!)
 {
-  saveBook(newBook:$newBook)
-  {_id username email savedBooks 
-    {bookId authors description title image link}
+  saveBook(bookData:$bookData)
+  {
+    _id 
+    username 
+    email 
+    bookCount
+    savedBooks 
+    {
+      bookId
+       authors
+        description 
+        title
+         image 
+         link
+        }
 }
 }
 `;
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId:ID!)
+mutation deleteBook($bookId:String!)
 {
-  removeBook(BookId:$bookId)
-  { _id username email savedBooks
-  {bookId authors description title image link}}
+  deleteBook(BookId:$bookId)
+  { 
+    _id 
+    username 
+    email
+    bookCount
+     savedBooks
+  {
+    bookId 
+    authors 
+    description 
+    title 
+    image
+     link
+    }}
 }`;
 
 
